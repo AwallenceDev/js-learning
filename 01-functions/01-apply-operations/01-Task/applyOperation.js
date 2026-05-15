f8d4c26dfdb692d90e5720f7f92aa56664929667
AwallenceDev <awallencedev@gmail.com>
2026-05-15 13:36:20 +0300

    A practice exercise on callbacks and arrow functions has been written in the function/applyOperation folder

--- 01-functions/01-apply-operations/01-Task/applyOperation.js
+++ 01-functions/01-apply-operations/01-Task/applyOperation.js
@@ -1,15 +1,11 @@
+const applyOperation = (num_1, num_2, callback) => callback(num_1, num_2);
+
 function main()
 {
-    const applyOperation = (num_1, num_2, callback) => 
-    {
-        return callback(num_1, num_2);
-    };
-
-    console.log(`${applyOperation(3, 5, (num_1,num_2) => num_1 + num_2)};`);
-    console.log(`${applyOperation(3, 5, (num_1,num_2) => num_1 - num_2)};`);
-    console.log(`${applyOperation(3, 5, (num_1,num_2) => num_1 * num_2)};`);
-    console.log(`${applyOperation(3, 5, (num_1,num_2) => num_1 / num_2)};`);
-
+    console.log(`Addition: ${applyOperation(3, 5, (a, b) => a + b)}`);
+    console.log(`Subtraction: ${applyOperation(3, 5, (a, b) => a - b)}`);
+    console.log(`Multiplication: ${applyOperation(3, 5, (a, b) => a * b)}`);
+    console.log(`Division: ${applyOperation(3, 5, (a, b) => a / b)}`);
 }
 
 main();
