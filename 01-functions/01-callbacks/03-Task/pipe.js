04c557e175c8d79d6945e76ad91a2a670ddea552
AwallenceDev <awallencedev@gmail.com>
2026-05-16 23:59:08 +0300

    feat: implement pipe function for chaining callbacks and add transformation example

---
+++ 01-functions/01-callbacks/03-Task/pipe.js
@@ -0,0 +1,24 @@
+const pipe = (callbacks) => 
+    {
+        return (value) => 
+            {
+
+            for (let i = 0; i < callbacks.length; i++)
+            {
+                callbacks(value);
+            } 
+            };
+    };
+
+const transform = pipe([
+    (element) => element * 2,
+    (element) => element + 10,
+    (element) => element ** 2,
+]);
+
+function main()
+{
+    transform(5);
+}
+
+main();
