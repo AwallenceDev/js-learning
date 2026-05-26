180682fee1041040588dc17894a9a5606f7bb4b0
AwallenceDev <awallencedev@gmail.com>
2026-05-26 20:36:16 +0300

    refactor: update validation logic in createValidator to use named functions for clarity and maintainability

--- 01-functions/01-callbacks/04-Task/createValidator.js
+++ 01-functions/01-callbacks/04-Task/createValidator.js
@@ -1,14 +1,20 @@
-const validate = createValidator([
-    (value) => value.length >= 3,
-    (value) => value.length <= 10,
-    (value) => value[0] === value[0].toUpperCase(),
+const isLongEnough = (userValue) => userValue.Length >=3; 
+
+const isShortEnough = (userValue) => userValue.Length <= 10; 
+
+const isCapitalized = (userValue) => value[0] === value[0].toUpperCase();
+
+const rules = createValidator([
+    isLongEnough,
+    isShortEnough,
+    isCapitalized,
 ]);
+
+
 
 function main()
 {
-    validate("Alex"); 
-    validate("Al");  
-    validate("Alexander the Great"); 
+    
 }
 
 main();
