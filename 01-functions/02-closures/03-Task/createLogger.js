2eb3fe8deea0d3556428c07d5c45090461e540d2
AwallenceDev <awallencedev@gmail.com>
2026-06-18 23:55:54 +0300

    refactor: remove logging messages from createLogger function

--- 01-functions/02-closures/03-Task/createLogger.js
+++ 01-functions/02-closures/03-Task/createLogger.js
@@ -1,10 +1,6 @@
 const createLogger = () =>
 {
-    const infoLog = createLogger("[INFO]");
-    const errorLog = createLogger("[ERROR]");
-
-    infoLog("Server started");   // [INFO] 12:00:00 — Server started
-    errorLog("Connection lost"); // [ERROR] 12:00:01 — Connection lost
+   
 }
 
 function main()
